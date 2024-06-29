eb13f30618257a94c29d8d0954258784f52deb88
Trainersudhanshu <124223047+Trainersudhanshu@users.noreply.github.com>
2024-06-29 13:32:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     @app.route('/')
-    def a():
-    print("hiiii")
-    x = 9; 
-    
     def home():
         return 'Hi Devops Geeks Welcome to the class123 New Data 123456'
 
